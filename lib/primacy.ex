4d2a641f7e41d1d18e5be31d2ad75748fb29147c
Matthew Miller <matt.miller@nfiindustries.com>
2021-02-18 16:16:06 -0600

    Add is_prime? checker

--- lib/primacy.ex
+++ lib/primacy.ex
@@ -1,18 +1,27 @@
 defmodule Primacy do
   @moduledoc """
-  Documentation for `Primacy`.
+  Prime number handling of various sorts
   """
 
   @doc """
-  Hello world.
+  Prime check.
 
   ## Examples
 
-      iex> Primacy.hello()
-      :world
+      iex> Primacy.is_prime?(61)
+      true
+
+      iex> Primacy.is_prime?(62)
+      false
 
   """
-  def hello do
-    :world
-  end
+  def is_prime?(n) when n < 2, do: false
+  def is_prime?(2), do: true
+  def is_prime?(n) when rem(n, 2) == 0, do: false
+  # Trial division should be good enough for our purposes
+  def is_prime?(n), do: is_prime?(n, 3)
+
+  defp is_prime?(n, k) when n < k * k, do: true
+  defp is_prime?(n, k) when rem(n, k) == 0, do: false
+  defp is_prime?(n, k), do: is_prime?(n, k + 2)
 end
--- test/primacy_test.exs
+++ test/primacy_test.exs
@@ -2,7 +2,10 @@ defmodule PrimacyTest do
   use ExUnit.Case
   doctest Primacy
 
-  test "greets the world" do
-    assert Primacy.hello() == :world
+  test "is_prime?" do
+    refute Primacy.is_prime?(-1)
+    refute Primacy.is_prime?(0)
+    refute Primacy.is_prime?(1)
+    assert Primacy.is_prime?(123_456_794_333)
   end
 end
