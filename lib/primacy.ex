e2cc0e9ed263ec77b2b99911dea54773207bf678
Matthew Miller <matt.miller@nfiindustries.com>
2021-02-18 04:31:28 -0600

    Initial skeleton commit

---
+++ lib/primacy.ex
@@ -0,0 +1,18 @@
+defmodule Primacy do
+  @moduledoc """
+  Documentation for `Primacy`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Primacy.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/primacy_test.exs
@@ -0,0 +1,8 @@
+defmodule PrimacyTest do
+  use ExUnit.Case
+  doctest Primacy
+
+  test "greets the world" do
+    assert Primacy.hello() == :world
+  end
+end
