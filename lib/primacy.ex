f1f595a1b39d51b0a997812dcc9a8d7c077722f5
Matthew Miller <matt.miller@nfiindustries.com>
2021-02-18 16:58:01 -0600

    Add primes_near/2 generator

--- lib/primacy.ex
+++ lib/primacy.ex
@@ -24,4 +24,63 @@ defmodule Primacy do
   defp is_prime?(n, k) when n < k * k, do: true
   defp is_prime?(n, k) when rem(n, k) == 0, do: false
   defp is_prime?(n, k), do: is_prime?(n, k + 2)
+
+  @doc """
+  Generate a list of primes near a given number
+
+  Options:
+
+  - `count`: how long a list to produce? (default: 1)
+  - `dir `:below`, `:above`, `:around` (default: `:around`)
+
+  ## Examples
+
+      iex> Primacy.primes_near(600)
+      [601]
+
+      iex> Primacy.primes_near(600, count: 10)
+      [571, 577, 587, 593, 599, 601, 607, 613, 617, 619]
+
+      iex> Primacy.primes_near(600, count: 10, dir: :below)
+      [541, 547, 557, 563, 569, 571, 577, 587, 593, 599]
+
+  """
+  def primes_near(n, opts \\ []) do
+    {c, d} = parse_options(opts)
+
+    {a, i} =
+      case is_prime?(n) do
+        true -> {[n], c - 1}
+        false -> {[], c}
+      end
+
+    down = fn c -> gather_primes(n - 1, c, -1, []) end
+    up = fn c -> gather_primes(n + 1, c, 1, []) end
+
+    case d do
+      :below ->
+        down.(i) ++ a
+
+      :above ->
+        a ++ up.(i)
+
+      :around ->
+        half = div(i, 2)
+        down.(half) ++ a ++ up.(half + rem(i, 2))
+    end
+  end
+
+  defp gather_primes(_, 0, 1, acc), do: Enum.reverse(acc)
+  defp gather_primes(_, 0, -1, acc), do: acc
+
+  defp gather_primes(n, c, i, acc) do
+    case is_prime?(n) do
+      false -> gather_primes(n + i, c, i, acc)
+      true -> gather_primes(n + i, c - 1, i, [n | acc])
+    end
+  end
+
+  defp parse_options(o) do
+    {Keyword.get(o, :count, 1), Keyword.get(o, :dir, :around)}
+  end
 end
--- test/primacy_test.exs
+++ test/primacy_test.exs
@@ -8,4 +8,12 @@ defmodule PrimacyTest do
     refute Primacy.is_prime?(1)
     assert Primacy.is_prime?(123_456_794_333)
   end
+
+  test "primes_near" do
+    assert [601] = Primacy.primes_near(601)
+    assert [601, 607] = Primacy.primes_near(601, count: 2)
+    assert [593, 599, 601] = Primacy.primes_near(601, count: 3, dir: :below)
+    assert [599, 601, 607] = Primacy.primes_near(601, count: 3, dir: :around)
+    assert [601, 607, 613] = Primacy.primes_near(601, count: 3, dir: :above)
+  end
 end
